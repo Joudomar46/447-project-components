071fca9cb268eba9ae66b5d910df86e1209edc27
Joudomar46 <77951776+Joudomar46@users.noreply.github.com>
2022-12-14 11:35:44 -0500

    Input Validation
    added input validation

--- src/pages/create/Form.js
+++ src/pages/create/Form.js
@@ -25,11 +25,95 @@ export function Form({ onSubmit }) {
   });
 
 
+  // for input validation
+  const [inputError, setInputError] = useState(false);
+  const [inputError1, setInputError1] = useState(false);
+  const [inputError2, setInputError2] = useState(false);
+  const [inputError3, setInputError3] = useState(false);
+  const [inputError4, setInputError4] = useState(false);
+  const [inputError5, setInputError5] = useState(false);
+  const [inputError6, setInputError6] = useState(false);
 
 
   // Handle submit
   function handleSubmit(event) {
     event.preventDefault();
+    let value = false;  //handling the full form input
+    setInputError(false);
+    setInputError1(false);
+    setInputError2(false);
+    setInputError3(false);
+    setInputError4(false);
+    setInputError5(false);
+    setInputError6(false);
+
+
+    // Validate the input
+    if (+panelDimention.panelWidth <= 0 ) {
+      // Input is empty, show an error message
+      setInputError('Enter a value greater than 0');
+      value = true;
+    }
+    if (+panelDimention.panelHeight <= 0 ) {
+      setInputError1('Enter a value greater than 0');
+      value = true;
+    }
+   
+    if (+roofDimention.roofWidth <= 0){
+      setInputError2('Enter a value greater than 0');
+      value = true;
+    }if (+roofDimention.roofWidth <= +panelDimention.panelWidth){
+        setInputError2('Roof width cannot be less than panel width');
+        value = true;
+    }if (+roofDimention.roofWidth <= +panelDimention.panelHeight){
+        setInputError2('Roof width cannot be less than panel height');
+        value = true;
+    }
+
+    if (+roofDimention.roofHeight <= 0){
+      setInputError3('Enter a value greater than 0');
+      value = true;
+    }if (+roofDimention.roofHeight <= +panelDimention.panelWidth){
+        setInputError3('Roof height cannot be less than panel width');
+        value = true;
+    }if (+roofDimention.roofHeight <= +panelDimention.panelHeight){
+        setInputError3('Roof height cannot be less than panel height');
+        value = true;
+    }
+
+    if (+spacingDimention.columnSpacing < 0){
+      setInputError4('Enter a value greater than or equal 0');
+      value = true;
+    } if (+spacingDimention.columnSpacing >= +roofDimention.roofWidth){
+      setInputError4('Column spacing cannot be greater than roof width');
+      value = true;
+    }
+
+    if (+spacingDimention.rowSpacing < 0){
+      setInputError5('Enter a value greater or equal to 0');
+      value = true;
+    } if (+spacingDimention.rowSpacing >= +roofDimention.roofHeight){
+      setInputError5('Row spacing cannot be greater than roof height');
+      value = true;
+    }
+
+    if (+spacingDimention.edgeSpacing < 0){
+      setInputError6('Enter a value greater than or equal to 0');
+      value = true;
+    } 
+
+    if(value === true){
+      return;
+    }
+
+    value = false;
+    setInputError(false);
+    setInputError1(false);
+    setInputError2(false);
+    setInputError3(false);
+    setInputError4(false);
+    setInputError5(false);
+    setInputError6(false);
 
     // return the updated values to Create
     onSubmit(panelDimention,roofDimention,spacingDimention);
@@ -38,7 +122,7 @@ export function Form({ onSubmit }) {
   return (
     
     <>
-     <Box   display="flex"
+     <Box display="flex"
   justifyContent="center"
   alignItems="center">
     <p>Enter the information bellow</p></Box>
@@ -60,7 +144,9 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
-            />
+              error={panelDimention.panelWidth !== "" && inputError}
+              helperText={panelDimention.panelWidth !== "" && inputError ? inputError : ' '}
+         />
           </Grid>
           <Grid item xs={6} sm={6}>
             <TextField
@@ -73,9 +159,11 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={panelDimention.panelHeight !== "" && inputError1}
+              helperText={panelDimention.panelHeight !== "" && inputError1 ? inputError1 : ' '}
             />
           </Grid>
-
+         
           <Grid item xs={6} sm={6}>
             <TextField
               name="roofWidth"
@@ -87,6 +175,8 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={roofDimention.roofWidth !== "" && inputError2}
+              helperText={roofDimention.roofWidth !== "" && inputError2 ? inputError2 : ' '}
             />
             </Grid>
             <Grid item xs={6} sm={6}>
@@ -100,6 +190,8 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={roofDimention.roofHeight !== "" && inputError3}
+              helperText={roofDimention.roofHeight !== "" && inputError3 ? inputError3 : ' '}
             />
             </Grid>
 
@@ -114,6 +206,8 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={spacingDimention.columnSpacing!== "" && inputError4}
+              helperText={spacingDimention.columnSpacing !== "" && inputError4 ? inputError4 : ' '}
             />
             </Grid>
             <Grid item xs={6} sm={6}>
@@ -127,6 +221,8 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={spacingDimention.rowSpacing!== "" && inputError5}
+              helperText={spacingDimention.rowSpacing !== "" && inputError5 ? inputError5 : ' '}
             />
             </Grid>
             <Grid item xs={6} sm={6}>
@@ -140,12 +236,14 @@ export function Form({ onSubmit }) {
                   [event.target.name]: event.target.value,
                 })
               }
+              error={spacingDimention.edgeSpacing!== "" && inputError6}
+              helperText={spacingDimention.edgeSpacing !== "" && inputError6 ? inputError6 : ' '}
             />
             </Grid>
 
           <Grid item xs={12}>
             <form/>
-            <Button   onClick={handleSubmit} type="submit" variant="contained" color="primary" >
+            <Button   onClick={handleSubmit} type="submit" variant="contained" color="primary">
               Submit
             </Button>
             <form/>
